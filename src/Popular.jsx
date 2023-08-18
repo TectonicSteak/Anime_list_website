3b3081fbce6ce124723dea72b8a6b72e5e5733dd
TectonicSteak <adithyasp2003@gmail.com>
2023-08-18 08:09:46 -0700

    new updates

--- src/Popular.jsx
+++ src/Popular.jsx
@@ -9,30 +9,32 @@ const API_URL = "https://api.jikan.moe/v4";
 const Popular = () =>{
     
     const [animes, setAnimes] =  useState([]);
-    const [searchTerm, setSearchTerm] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
     const popularAnime = async () =>{
         const response = await fetch(`${API_URL}/top/anime?filter=bypopularity`);
         const data = await response.json();
         
         setAnimes(data.data);
-        console.log(animes);
     }
 
     const searchAnime = async (title) =>{
-        const response = await fetch(`${API_URL}/anime?letter`);
+        const response = await fetch(`${API_URL}/anime?q=${title}`);
         const data = await response.json();
         
         setAnimes(data.data);
     }
     
+    console.log(animes);
+
     useEffect(() =>{
         popularAnime();
     },[]);
 
+
     return(
         <div className="app">
-            <h1>Otaku</h1>
+            <h1 onClick={popularAnime}>Otaku</h1>
 
             <div className="search">
                 <input
@@ -43,7 +45,10 @@ const Popular = () =>{
                 <img 
                     src={SearchIcon}
                     alt='search'
-                    onClick={() => {}}
+                    onClick={(e) => {
+                        searchAnime(searchTerm)
+                        console.log(searchTerm);
+                    }}
                 />
             </div>
             
@@ -52,7 +57,9 @@ const Popular = () =>{
                     ? (
                         <div className="container">
                             {animes.map((anime) => (
-                                <AnimeCard Anime={anime}/>
+                                <a key={anime.mal_id} href="./AnimePage">
+                                    <AnimeCard Anime={anime}/>
+                                </a>
                             ))};
                         </div>
                     ):(
@@ -61,6 +68,9 @@ const Popular = () =>{
                         </div>
                     )
             }
+
+            <button className="load-more">Load More</button>
+
         </div>
     );
 }
